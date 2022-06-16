1667a1f391e38b246d7e904393e07460f8541805
Mel Peto <melanie.peto@gmail.com>
2022-06-16 11:12:20 -0400

    hello

--- src/App.js
+++ src/App.js
@@ -4,18 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>hello world</h1>
         <p>Mel is changing this site</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
