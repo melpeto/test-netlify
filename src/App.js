09b28c40370a70434bddd6fba43726627bb3151e
Mel Peto <melanie.peto@gmail.com>
2022-06-16 11:08:33 -0400

    made a lil change to test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Mel is changing this site</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
